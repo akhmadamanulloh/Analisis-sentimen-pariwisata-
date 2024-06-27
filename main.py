6fb535b8e5525dd6b250544f375e6496633ef1c9
Akhmad Amanulloh <84214955+akhmadamanulloh@users.noreply.github.com>
2024-06-27 11:24:02 +0700

    Update main.py

--- main.py
+++ main.py
@@ -91,8 +91,8 @@ st.title("Analisis Sentimen Destinasi Pariwisata Melalui Ulasan Google Maps Meng
 with st.sidebar:
     selected = option_menu(
         menu_title="Menu",
-        options=["Home", "Dataset", "Prediksi Teks Tunggal", "Prediksi Batch dari CSV", "Grafik"],
-        icons=["house", "table", "file-text", "file-text", "bar-chart"],
+        options=["Home", "Dataset", "Prediksi Ulasan", "Prediksi Ulasan CSV"],
+        icons=["house", "table", "file-text", "file-text"],
         menu_icon="cast",
         default_index=0,
     )
@@ -178,7 +178,7 @@ elif selected == "Prediksi Teks Tunggal":
 
 # Prediksi Batch dari CSV
 elif selected == "Prediksi Batch dari CSV":
-    st.subheader("Prediksi Batch dari CSV")
+    st.subheader("Prediksi Ulasan dari CSV")
     uploaded_file = st.file_uploader("Unggah file CSV", type="csv")
     feature_selection_method = st.selectbox("Pilih Metode Seleksi Fitur", ["Information Gain", "Chi-Square", "Kombinasi Seleksi Fitur"])
     
@@ -217,38 +217,3 @@ elif selected == "Prediksi Batch dari CSV":
             # Unduh hasil prediksi
             csv = df_uploaded.to_csv(index=False)
             st.download_button(label="Unduh Prediksi", data=csv, file_name='predictions.csv', mime='text/csv')
-
-# Grafik
-elif selected == "Grafik":
-    
-    st.write("### Grafik Information Gain")
-    st.image("information_gain.jpg")
-    st.write("""
-        <div style="text-align:justify;">
-        Pada skenario ini dilakukan pengujian untuk mencari akurasi dan waktu. pengujian dilakukan menggunakan SMOTE Selanjutnya dilakukan seleksi fitur information gain. Selanjutnya dilakukan pemodelan Support Vector menggunakan best hyperparameter. Pada pengujian ini menggunakan variasi jumlah fitur sebesar 95%, 90%, 85% 80%, 75%, 70%, 65%, 60%, 55%, 50%, 45%, 40 %, 35%, 30%, 25%, 20%, 15%, 10%, 5% dari keseluruhan fitur yang ada.
-        <br>Akurasi dan waktu terbaik terdapat di variasi jumlah fitur sebesar 50% dengan akurasi 95% dan total waktu 57,59 detik.
-        </div>
-    """, unsafe_allow_html=True)
-    st.image("word posotif ig.jpg")
-    
-    st.write("### Grafik Chi-Square")
-    st.image("chi_square.jpg")
-    st.write("""
-        <div style="text-align:justify;">
-        Pada skenario ini dilakukan pengujian untuk mencari akurasi dan waktu. pengujian dilakukan menggunakan SMOTE Selanjutnya dilakukan seleksi fitur Chi Square. Selanjutnya dilakukan pemodelan Support Vector menggunakan best hyperparameter. Pada pengujian ini menggunakan variasi jumlah fitur sebesar 95%, 90%, 85% 80%, 75%, 70%, 65%, 60%, 55%, 50%, 45%, 40 %, 35%, 30%, 25%, 20%, 15%, 10%, 5% dari keseluruhan fitur yang ada.
-        <br>Pada gambar diatas akurasi dan waktu terbaik terdapat di variasi jumlah fitur sebesar 20% dengan akurasi 95,43% dan total waktu 21,15 detik.
-        </div>
-    """, unsafe_allow_html=True)
-    st.image("word posotif chi.jpg")
-    st.image("word negatif chi.jpg")
-    
-    st.write("### Grafik Kombinasi Seleksi Fitur")
-    st.image("kombinasi_seleksi_fitur.jpg")
-    st.write("""
-        <div style="text-align:justify;">
-        Pada skenario ini dilakukan pengujian untuk mencari akurasi dan waktu. pengujian dilakukan menggunakan SMOTE Selanjutnya dilakukan pemodelan Support Vector dengan kombinasi seleksi fitur information gain dan Chi Square. Pada pengujian ini akan dilakukan kombinasi dengan menggunakan variasi jumlah fitur sebesar 95%, 90%, 85% 80%, 75%, 70%, 65%, 60%, 55%, 50%, 45%, 40 %, 35%, 30%, 25%, 20%, 15%, 10%, 5% dari keseluruhan fitur yang ada
-        <br>Pada gambar diatas akurasi dan waktu terbaik terdapat di variasi jumlah fitur sebesar 60% dengan akurasi 95,02% dan total waktu 47,66 detik.
-        </div>
-    """, unsafe_allow_html=True)
-    st.image("word posotif kombinasi.jpg")
-    st.image("word negatif kombinasi.jpg")
